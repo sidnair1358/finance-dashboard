fc959dae2bb0642cf52c1f58daeca4802c4c45f7
sidn <siddharth.nair@bravissimo.com>
2025-04-08 11:11:23 +0100

    Add custom height for medium screens

--- src/app/pages/accounts/page.js
+++ src/app/pages/accounts/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Accounts = () => {
-  return <div className="h-[60vh]">Accounts</div>;
+  return <div className="h-[80vh] lg:h-[60vh]">Accounts</div>;
 };
 
 export default Accounts;
